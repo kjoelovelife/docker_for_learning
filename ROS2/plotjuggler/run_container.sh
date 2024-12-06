27dcd7b41c8e61881520de1f0636055bece2460f
Joe Lin <joe_lin@brogent.com>
2024-12-06 11:06:38 +0800

    Add: Dockerfile for plotjuggler

---
+++ ROS2/plotjuggler/run_container.sh
@@ -0,0 +1,11 @@
+unset CONTAINER_PLOTJUGGLER
+CONTAINER_PLOTJUGGLER="ros2-humble-plotjuggler"
+
+# ensure the Host X Server is Accessible
+xhost +local:docker
+
+# Pass the DISPLAY Variable to the Container
+sudo docker container run -it --rm \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  ${CONTAINER_PLOTJUGGLER}
